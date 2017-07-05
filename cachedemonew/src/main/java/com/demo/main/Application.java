2069bfd06b2c39ac69c16e7b35c4fee52b32deed
B SRIDHAR <Sridharb.in@gmail.com>
2017-07-06 02:35:03 +0530

    changes

--- cachedemonew/src/main/java/com/demo/main/Application.java
+++ cachedemonew/src/main/java/com/demo/main/Application.java
@@ -2,9 +2,6 @@ package com.demo.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -13,14 +10,6 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 	
-	@Bean
-	public AlwaysSampler defaultSampler() {
-	  return new AlwaysSampler();
-	}
 	
-	@Bean
-	public RestTemplate getRestTemplate() {
-	    return new RestTemplate();
-	}
 	
 }
--- cachedemonew/src/main/java/com/demo/main/service/SimpleService.java
+++ cachedemonew/src/main/java/com/demo/main/service/SimpleService.java
@@ -3,8 +3,6 @@ package com.demo.main.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.sleuth.Span;
-import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -17,36 +15,7 @@ public class SimpleService {
 	
 	
 
-	@Autowired
-	private RestTemplate restTemplate;
-
-	@Autowired
-	private Tracer tracer;
 	
-	public void doSomeWorkNewSpan() throws InterruptedException {
-	    logger.info("I'm in the original span");
-	 
-	    Span newSpan = tracer.createSpan("newSpan");
-	    try {
-	        Thread.sleep(1000L);
-	        logger.info("I'm in the new span doing some cool work that needs its own span");
-	    } finally {
-	        tracer.close(newSpan);
-	    }
-	 
-	    logger.info("I'm in the original span");
-	}
-	@Async
-	public void asyncMethod()  {
-	    logger.info("Start Async Method");
-	    try {
-			Thread.sleep(1000L);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    logger.info("End Async Method");
-	}
 	//@Cacheable(value = "employee")
 	public String getByEmployeeNo(String empNo) throws InterruptedException {
 		logger.info("Inside getByEmployeeNo in SimpleService");
